944fe456e2df35112067b0d41b49783b7830f91d
guelmamihiba <guelmamihiba1@gmail.com>
2024-02-22 10:37:11 +0100

    question table done

--- Backend/controller/qustions.js
+++ Backend/controller/qustions.js
@@ -1,33 +1,46 @@
 const questions=require("../models/questions")
 
 module.exports = {
-    getAll: function(callback) {
-      pool.query('SELECT * FROM questions', function(err, results) {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, results);
-        }
+  getAllQuestions: (req, res) => {
+      questions.getAll(function(err, results) {
+          if (err) {
+              res.status(500).send(err);
+          } else {
+              res.json(results);
+          }
       });
-    },
-  
-    create: function(newQuestion, callback) {
-      pool.query('INSERT INTO questions SET ?', newQuestion, function(err, result) {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, { id: result.insertId, ...newQuestion });
-        }
+  },
+  addQuestion: function(req, res) {
+      var newQuestion = req.body;
+      questions.create(newQuestion, function(err, results) {
+          if (err) {
+              res.status(500).send(err);
+          } else {
+              res.json(results);
+          }
       });
-    },
-  
-    remove: function(questionId, callback) {
-      pool.query('DELETE FROM questions WHERE id = ?', questionId, function(err, result) {
-        if (err) {
-          callback(err, null);
-        } else {
-          callback(null, result.affectedRows > 0);
-        }
+  },
+  updateQuestion: (req, res) => {
+      var updatedQuestion = req.body;
+      var questionId = req.params.id;
+
+      questions.update(questionId, updatedQuestion, function(err, results) {
+          if (err) {
+              res.status(500).send(err);
+          } else {
+              res.json(results);
+          }
       });
-    },
-}  
+  },
+  deleteQuestion: (req, res) => {
+      var questionId = req.params.id;
+
+      questions.remove(questionId, function(err, results) {
+          if (err) {
+              res.status(500).send(err);
+          } else {
+              res.json(results);
+          }
+      });
+  }
+};
