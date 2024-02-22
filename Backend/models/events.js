c6652f2548ce53e160bea6c30146a8d074ad0499
khalilcherni <kcherni411@gmail.com>
2024-02-22 11:10:52 +0100

    14

--- Backend/models/events.js
+++ Backend/models/events.js
@@ -12,5 +12,11 @@ module.exports={
         connection.query(sql,x,function(error,results){
             callback(error,results)
         })
-    }
+    },
+    updateevents : (params, id, callback) => {
+        const sql = 'UPDATE events SET `name`=?, `image`=?, `description`=? ,`location`=? ,`type`=? ,`startdate`=?,`enddate`=?,`registrationdeadline`=?  WHERE `id`=?';
+        connection.query(sql, [params.name, params.description, params.image,params.location,params.type,params.startdate,params.enddate,params.registrationdeadline, id], function(err, results) {
+          callback(err, results);
+        });
+      }
 }
--- Backend/routes/events.js
+++ Backend/routes/events.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
 const router = express.Router();
-const { getALLevents, addevent } = require("../controller/events");
+const { getALLevents, addevent, update } = require("../controller/events");
 
 
 router.get('/get', getALLevents);
 router.post('/add',addevent)
+router.put('/put/:id',update)
 module.exports=router
