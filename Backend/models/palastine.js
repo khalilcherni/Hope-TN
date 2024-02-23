e1538223bdc0676c3ae32450de52afdcba36ea9a
AmirOuerghi <ouerghi501@gmail.com>
2024-02-23 13:20:47 +0100

    moving to testing

--- Backend/models/palastine.js
+++ Backend/models/palastine.js
@@ -1,47 +1,47 @@
-// models/PalastineModel.js
+// models/palestineModel.js
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_mysql_username',
-  password: 'your_mysql_password',
-  database: 'charity'
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'your_mysql_username',
+//   password: 'your_mysql_password',
+//   database: 'charity'
+// });
 
-connection.connect();
+// connection.connect();
 
-const createPalastine = (data, callback) => {
-  const { name, description, location, phone } = data;
-  const query = `INSERT INTO Palastine (name, description, location, phone) VALUES (?, ?, ?, ?)`;
-  connection.query(query, [name, description, location, phone], callback);
+const createPalestine = (data, callback) => {
+  const { image, description } = data;
+  const query = `INSERT INTO palestine (image, description) VALUES (?, ?)`;
+  connection.query(query, [image, description], callback);
 };
 
-const getAllPalastine = (callback) => {
-  const query = `SELECT * FROM Palastine`;
+const getAllPalestine = (callback) => {
+  const query = `SELECT * FROM palestine`;
   connection.query(query, callback);
 };
 
-const getPalastineById = (id, callback) => {
-  const query = `SELECT * FROM Palastine WHERE id = ?`;
+const getPalestineById = (id, callback) => {
+  const query = `SELECT * FROM palestine WHERE id = ?`;
   connection.query(query, [id], callback);
 };
 
-const updatePalastine = (id, data, callback) => {
-  const { name, description, location, phone } = data;
-  const query = `UPDATE Palastine SET name = ?, description = ?, location = ?, phone = ? WHERE id = ?`;
-  connection.query(query, [name, description, location, phone, id], callback);
+const updatePalestine = (id, data, callback) => {
+  const { image, description } = data;
+  const query = `UPDATE palestine SET image = ?, description = ? WHERE id = ?`;
+  connection.query(query, [image, description, id], callback);
 };
 
-const deletePalastine = (id, callback) => {
-  const query = `DELETE FROM Palastine WHERE id = ?`;
+const deletePalestine = (id, callback) => {
+  const query = `DELETE FROM palestine WHERE id = ?`;
   connection.query(query, [id], callback);
 };
 
 module.exports = {
-  createPalastine,
-  getAllPalastine,
-  getPalastineById,
-  updatePalastine,
-  deletePalastine
+  createPalestine,
+  getAllPalestine,
+  getPalestineById,
+  updatePalestine,
+  deletePalestine
 };
