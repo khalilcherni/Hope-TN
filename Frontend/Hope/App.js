95105ea1d80550cf4ca380a1a5c5e3bc1aeff2a2
khalilcherni <kcherni411@gmail.com>
2024-02-27 10:36:13 +0100

    57

--- Frontend/Hope/App.js
+++ Frontend/Hope/App.js
@@ -9,14 +9,12 @@ import OnboardingPage1 from './screens/onboardingPage1';
 import OnboardingPage2 from './screens/OnboardingPage2';
 import SignIn from './screens/signin';
 import Home from './screens/Home';
-
-import SignIn  from './screens/signin';
 // import StartingPage from './screens/StartingPage';
 // import StartingPage from './component/StartingPage'; // Import StartingPage component
 // import SignUp from './screens/signup';
 // import OnboardingPage from './screens/onboardingPage1';
 // import SignIn  from './screens/signin';
-import Home from "./screens/Home";
+
 const Stack = createStackNavigator();
 
 export default function App() {
