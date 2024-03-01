7e3998f80a52cb93c9870d605c548779528e3e67
khalilcherni <kcherni411@gmail.com>
2024-03-01 15:06:05 +0100

    93

--- Frontend/Hope/screens/ChatRoom.jsx
+++ Frontend/Hope/screens/ChatRoom.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 import { View, StyleSheet,TouchableOpacity,Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 const Chatroom = ({ userId }) => {
   const [messages, setMessages] = useState([]);
-
+  const navigation = useNavigation();
   useEffect(() => {
     loadMessages();
   }, []);
@@ -37,7 +40,22 @@ const Chatroom = ({ userId }) => {
     const updatedMessages = GiftedChat.append(messages, newMessages);
     setMessages(updatedMessages);
   };
-
+  const handleHomeNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('Home');
+  };
+  const handleChatNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('ChatRoom');
+  };
+  const handleSchoolNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('School');
+  };
+  const handleMESNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('Messages');
+  };
   return (
     <View style={styles.container}>
       <View style={styles.backgroundContainer} />
@@ -51,13 +69,11 @@ const Chatroom = ({ userId }) => {
         style={styles.giftedChat}
       />
        <View style={styles.tabbar}>
-          <TouchableOpacity style={styles.tabItem} onPress={() => handleTabPress('Home')}><Text>Home</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.tabItem} onPress={() => handleTabPress('ChatRoom')}><Text>ChatRoom</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.tabItem} onPress={() => handleTabPress('Messages')}><Text>Messages</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.tabItem} onPress={() => handleTabPress('Setting')}><Text>Setting</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.tabItem} onPress={() => handleTabPress('Setting')}><Text>Setting</Text></TouchableOpacity>
-          
-        </View>
+        <TouchableOpacity style={styles.tabItem} onPress={handleHomeNavigation}><AntDesign name="home" size={24} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem}onPress={handleChatNavigation}><Ionicons name="chatbox-ellipses-outline" size={24} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem} onPress={handleSchoolNavigation}><MaterialCommunityIcons name="school-outline" size={24} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem}onPress={handleMESNavigation}><MaterialCommunityIcons name="android-messages" size={24} color="black" /></TouchableOpacity>
+      </View>
     </View>
   );
 };
