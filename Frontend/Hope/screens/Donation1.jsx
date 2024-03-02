7c81387d0d44b60f235d1e1f8c0efa45c201c603
khalilcherni <kcherni411@gmail.com>
2024-03-02 19:00:23 +0100

    101

--- Frontend/Hope/screens/Donation1.jsx
+++ Frontend/Hope/screens/Donation1.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, Dimensions, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+const windowWidth = Dimensions.get('window').width;
 
 const DonationScreen = ({ route }) => {
   const navigation = useNavigation(); // Initialize the useNavigation hook
@@ -12,7 +16,7 @@ const DonationScreen = ({ route }) => {
     if (enteredAmount) {
       const amount = parseInt(enteredAmount);
       handleDonate(amount, cardId);
-      Alert.alert("Donation done", "Rabi ykather khirk", [
+      Alert.alert("Donation done", "Thank you for your donation", [
         {
           text: "OK",
           onPress: () => navigation.goBack(), // Navigate back to the CharityScreen
@@ -22,44 +26,67 @@ const DonationScreen = ({ route }) => {
       Alert.alert("Please enter a donation amount");
     }
   };
+  const handleHomeNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('Home');
+  };
+  const handleChatNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('ChatRoom');
+  };
+  const handleSchoolNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('School');
+  };
+  const handleMESNavigation = () => {
+    // Navigate to the Home screen
+    navigation.navigate('Messages');
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Donation</Text>
-      <TextInput
-      placeholder='Name'
-      style={styles.input}
-      />
-       <TextInput
-      placeholder='City'
-      style={styles.input}
-      />
-       <TextInput
-      placeholder='phone'
-      style={styles.input}
-      />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <Text style={styles.heading}>Donation</Text>
         <TextInput
-      placeholder='Card'
-      style={styles.input}
-      />
+          placeholder='Name'
+          style={styles.input}
+        />
         <TextInput
-      placeholder='Card Number'
-      style={styles.input}
-      />
-      <TextInput
-        placeholder="amount"
-    
-        style={styles.input}
-        keyboardType="numeric"
-        value={enteredAmount}
-        onChangeText={setEnteredAmount}
-      />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handleDonateAmount}
-      >
-        <Text style={styles.buttonText}>Donate</Text>
-      </TouchableOpacity>
+          placeholder='City'
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Phone'
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Card'
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='Card Number'
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Amount"
+          style={styles.input}
+          keyboardType="numeric"
+          value={enteredAmount}
+          onChangeText={setEnteredAmount}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleDonateAmount}
+        >
+          <Text style={styles.buttonText}>Donate</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <View style={styles.tabbar}>
+        <TouchableOpacity style={styles.tabItem} onPress={handleHomeNavigation}><AntDesign name="home" size={24} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem} onPress={handleChatNavigation}><Ionicons name="chatbox-ellipses-outline" size={24} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem} onPress={handleSchoolNavigation}><MaterialCommunityIcons name="school-outline" size={24} color="black" /></TouchableOpacity>
+        <TouchableOpacity style={styles.tabItem} onPress={handleMESNavigation}><MaterialCommunityIcons name="android-messages" size={24} color="black" /></TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -67,9 +94,13 @@ const DonationScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  scrollViewContent: {
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f0f0f0',
+    paddingBottom: 80, // Adjust this value according to the tab bar height
   },
   heading: {
     fontSize: 24,
@@ -79,22 +110,22 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 55,
+    width: windowWidth * 0.8,
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
-    backgroundColor:  '#209FA6',
-    width: 290,
+    backgroundColor: '#209FA6',
     borderRadius: 30,
     textAlign: 'center',
     marginBottom: 20,
   },
   button: {
     height: 55,
+    width: windowWidth * 0.8,
     borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
     backgroundColor: '#209FA6',
-    width: 290,
     borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
@@ -104,6 +135,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
+  },
+  tabbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#f0f0f0',
+    paddingVertical: 10,
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: 'center',
   },
 });
 
